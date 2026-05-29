c4b20e4110712f29d2d84003515f11c2c05febf9
nnra6864 <nnra2210@gmail.com>
2026-05-29 11:07:19 +0200

    Removed threshold

--- cursor_warp.glsl
+++ cursor_warp.glsl
@@ -7,7 +7,7 @@ vec3 sRGBToLinear(vec3 c) {
 vec4 TRAIL_COLOR = vec4(sRGBToLinear(iCurrentCursorColor.rgb), iCurrentCursorColor.a); // for custom color: vec4(0.2, 0.6, 1.0, 0.5); (wrap in sRGBToLinear for correct brightness)
 const float DURATION = 0.2; // total animation time
 const float TRAIL_SIZE = 0.8; // 0.0 = all corners move together. 1.0 = max smear (leading corners jump instantly)
-const float THRESHOLD_MIN_DISTANCE = 1.5; // min distance to show trail (units of cursor height)
+const float THRESHOLD_MIN_DISTANCE = 0; // min distance to show trail (units of cursor height)
 const float BLUR = 1.0; // blur size in pixels (for antialiasing)
 const float TRAIL_THICKNESS = 1.0;  // 1.0 = full cursor height, 0.0 = zero height, >1.0 = funky aah
 const float TRAIL_THICKNESS_X = 0.9;
